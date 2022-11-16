14e8a09cff119052464dd20371fc0e6714b2aad4
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2022-11-16 09:21:38 -0500

    add basic data graphing script

---
+++ scripts/graph_data.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import argparse
+import csv
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath")
+    args = parser.parse_args()
+    
+    with open(args.filepath, "r") as f:
+        reader = csv.reader(f)
+        data = []
+        for row in reader:
+            try:
+                row = [int(ele) for ele in row]
+                if len(row) == 4:
+                    data.append(row)
+            except Exception as e:
+                print(f"Exception adding row to data: {e}")    
+
+        print(data)
+        x = [row[1] for row in data]
+        y = [row[3] for row in data]
+        p = plt.plot(x, y)
+        plt.show()
