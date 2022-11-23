50d9b19b9f8c4e9270f053f51380f5ca1e15e649
Erfan Huq <erfanshazid@gmail.com>
2022-11-23 18:49:17 -0500

    position tracking and numerical integration

--- stm32/Core/Inc/constants.h
+++ stm32/Core/Inc/constants.h
@@ -9,15 +9,19 @@
 #define INC_CONSTANTS_H_
 
 #define MS_PER_SEC 1000.0
+#define RADIONS_TO_DEGREES 57.2958
+
+#define SIDE_TOF_SEPARATION_MM 177 // Distance between the two side ToF sensors. TODO revise this
 
 // ToF calibration info.
-#define STOPPING_DISTANCE_MM 240
+#define STOPPING_DISTANCE_MM 160 // original - 240 TODO: Tuning
 #define BOARD_SQUARE_SIZE_MM 300 // original - 300
 #define TOF_BASE_SIDE_DIST_MM 100 // distance from the side TOFs to the edge of the board. Need to revisit.
 
 // Motor speed values.
 #define BASE_MOTOR_SPEED 100
 #define TURNING_MOTOR_SPEED 100
+#define MIN_MOTOR_SPEED 30
 
 #define MOTOR_BRAKE_SPEED 30
 #define MOTOR_BRAKE_DELAY 175
@@ -28,8 +32,8 @@
 // IMU turning constants
 #define IMU_TURN_ERROR_THRESH 5
 #define IMU_TURN_MIN_TIME 750
-#define IMU_TURN_CORRECTION_SPEED (TURNING_MOTOR_SPEED / 2)
 #define IMU_TURN_MIN_FULL_SPEED_ERROR 30
+#define IMU_TURN_CORRECTION_SPEED (TURNING_MOTOR_SPEED / 2)
 
 // Course correction values
 #define CORRECTION_FACTOR 10
@@ -39,7 +43,14 @@
 #define PHOTORESISTOR_THRESHOLD 2300
 
 // Turning Correction Values
-#define ANGLE_CORRECTION_THRESHOLD 15
-#define RADIONS_TO_DEGREES 57.2958
+#define ANGLE_CORRECTION_ADJUSTMENT_THRESHOLD 5
+#define TURN_CORRECTION_MOTOR_SPEED (TURNING_MOTOR_SPEED / 2)
+#define TOF_ANGLE_CORRECTION_THRESHOLD 20
+
+// Controlled Stopping constants
+#define CC_KP 0.003f // Proportional coefficient (unused)
+#define CC_KI 0.005f // Derivative coefficient (unused)
+#define CONTROLLED_STOP_RANGE 25 // TODO: Tuning
+#define CONTROLLED_STOP_DISTANCE_CORRECTION 60
 
 #endif /* INC_CONSTANTS_H_ */
